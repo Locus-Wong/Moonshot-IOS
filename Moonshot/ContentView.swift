28e5bf10d9a1007a8a9695ebb96156b76315a5a3
Locus Wong <wongtzswa@gmail.com>
2026-02-27 23:22:43 -0500

    update gitignore

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Locus Wong on 2026-02-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Locus Wong on 2026-02-27.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
